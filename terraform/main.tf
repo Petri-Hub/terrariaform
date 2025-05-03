24f6827e40b12687873426103ef17579336f19f3
Petri-Hub <fernando.petri01@gmail.com>
2025-05-03 12:00:35 -0300

    ref: adjust 'main.tf' file resources ordering

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,17 +11,6 @@ provider "vultr" {
   api_key = var.api_key
 }
 
-resource "vultr_ssh_key" "terrariaform_ssh_key" {
-  name    = "terrariaform-key"
-  ssh_key = var.public_ssh_key
-}
-
-resource "vultr_startup_script" "terrariaform-startup-script" {
-  name        = "terrariaform-startup-script"
-  script      = base64encode(file("../scripts/startup.sh"))
-  type        = "boot"
-}
-
 resource "vultr_instance" "terrariaform-server" {
   label = "terrariaform-server"
   plan = var.instance
@@ -30,3 +19,17 @@ resource "vultr_instance" "terrariaform-server" {
   script_id = vultr_startup_script.terrariaform-startup-script.id
   ssh_key_ids = [vultr_ssh_key.terrariaform_ssh_key.id]
 }
+
+resource "vultr_startup_script" "terrariaform-startup-script" {
+  name        = "terrariaform-startup-script"
+  script      = base64encode(file("../scripts/startup.sh"))
+  type        = "boot"
+}
+
+resource "vultr_ssh_key" "terrariaform_ssh_key" {
+  name    = "terrariaform-key"
+  ssh_key = var.public_ssh_key
+}
+
+
+

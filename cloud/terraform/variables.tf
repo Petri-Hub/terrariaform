4d1127b995d786b6e7550c948abda338d4b21e77
Petri-Hub <fernando.petri01@gmail.com>
2025-05-10 16:58:51 -0300

    chore: remove non-existent variables from Terraform variables files

--- cloud/terraform/variables.tf
+++ cloud/terraform/variables.tf
@@ -1,26 +1,4 @@
-variable "api_key" {
-  description = "Vultr API key"
-  type        = string
-  sensitive   = true  
-}
-
 variable "region" {
-  description = "Vultr region"
+  description = "AWS region"
   type        = string
-}
-
-variable "instance" {
-  description = "Vultr instance type"
-  type        = string
-}
-
-variable "os" {
-  description = "Vultr OS ID"
-  type        = string
-}
-
-variable "public_ssh_key" {
-  description = "Public SSH key for Vultr instance"
-  type        = string
-  sensitive = true
 }

7f2fc9abf2629198938775f2a6cd3db9aae51b4a
Petri-Hub <fernando.petri01@gmail.com>
2025-05-12 20:09:04 -0300

    feat: add basic instance without network security

--- cloud/terraform/main.tf
+++ cloud/terraform/main.tf
@@ -10,3 +10,36 @@ terraform {
 provider "aws" {
   region = var.region
 }
+
+data "aws_ami" "terrariaform-ami" {
+  most_recent = true
+  owners      = ["136693071363"] # Debian project owner ID
+  filter {
+    name   = "name"
+    values = ["debian-12-amd64-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+resource "aws_ami" "terrariaform-ami" {
+  name = "terrariaform-ami"
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.debian12.id
+  instance_type = var.instance_type
+  
+  tags = {
+    Name = "terrariaform-server"
+  }
+
+  user_data = file("../scripts/startup.sh")
+  user_data_replace_on_change = true
+}

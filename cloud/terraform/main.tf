5da45163afac846742a8d48c62a7b468216fb89b
Petri-Hub <fernando.petri01@gmail.com>
2025-05-12 20:39:44 -0300

    feat: setup AWS basic network configuration

--- cloud/terraform/main.tf
+++ cloud/terraform/main.tf
@@ -11,6 +11,29 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_security_group" "terrariaform-sg" {
+  name = "terrariaform-sg"
+  description = "Security group that enables SSH and game connections"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "terrariaform-ssh-allow" {
+  description = "Allow SSH access"
+  security_group_id = aws_security_group.terrariaform-sg.id
+  from_port         = 22
+  to_port           = 22
+  ip_protocol = "tcp"
+  cidr_ipv4 = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "terrariaform-game-allow" {
+  description = "Allow game connections"
+  security_group_id = aws_security_group.terrariaform-sg.id
+  from_port         = 7777
+  to_port           = 7777
+  ip_protocol = "tcp"
+  cidr_ipv4 = "0.0.0.0/0"
+}
+
 data "aws_ami" "terrariaform-ami" {
   most_recent = true
   owners      = ["136693071363"] # Debian project owner ID
@@ -24,21 +47,19 @@ data "aws_ami" "terrariaform-ami" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-  
+
   filter {
     name   = "root-device-type"
     values = ["ebs"]
   }
 }
 
-resource "aws_ami" "terrariaform-ami" {
-  name = "terrariaform-ami"
-}
-
 resource "aws_instance" "example" {
   ami           = data.aws_ami.terrariaform-ami.id
   instance_type = var.instance_type
   
+  vpc_security_group_ids = [aws_security_group.terrariaform-sg.id]
+
   tags = {
     Name = "terrariaform-server"
   }

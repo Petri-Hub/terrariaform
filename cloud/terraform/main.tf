6c97e1260c7e1c4ad2c8a1043a40d62a37c0277e
Petri-Hub <fernando.petri01@gmail.com>
2025-05-16 20:44:21 -0300

    ref: rename resource names to use snake case

--- cloud/terraform/main.tf
+++ cloud/terraform/main.tf
@@ -11,46 +11,46 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_eip" "terrariaform-elastic-ip" {
-  instance = aws_instance.example.id
+resource "aws_eip" "terrariaform_elastic_ip" {
+  instance = aws_instance.terrariaform_server.id
 }
 
-resource "aws_security_group" "terrariaform-sg" {
+resource "aws_security_group" "terrariaform_sg" {
   name = "terrariaform-sg"
   description = "Security group that enables SSH and game connections"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "terrariaform-ssh-allow" {
+resource "aws_vpc_security_group_ingress_rule" "terrariaform_ssh_allow" {
   description = "Allow SSH access"
-  security_group_id = aws_security_group.terrariaform-sg.id
+  security_group_id = aws_security_group.terrariaform_sg.id
   from_port         = 22
   to_port           = 22
   ip_protocol = "tcp"
   cidr_ipv4 = "0.0.0.0/0"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "terrariaform-game-allow" {
+resource "aws_vpc_security_group_ingress_rule" "terrariaform_game_allow" {
   description = "Allow game connections"
-  security_group_id = aws_security_group.terrariaform-sg.id
+  security_group_id = aws_security_group.terrariaform_sg.id
   from_port         = 7777
   to_port           = 7777
   ip_protocol = "tcp"
   cidr_ipv4 = "0.0.0.0/0"
 }
 
-resource "aws_vpc_security_group_egress_rule" "terrariaform-all-egress" {
+resource "aws_vpc_security_group_egress_rule" "terrariaform_all_egress" {
   description       = "Allow all outbound traffic"
-  security_group_id = aws_security_group.terrariaform-sg.id
+  security_group_id = aws_security_group.terrariaform_sg.id
   ip_protocol       = "-1"
   cidr_ipv4         = "0.0.0.0/0"
 }
 
-resource "aws_key_pair" "terrariaform-ssh-key" {
+resource "aws_key_pair" "terrariaform_ssh_key" {
   key_name   = "terrariaform-ssh-key"
   public_key = file(var.ssh_key_path)
 }
 
-data "aws_ami" "terrariaform-ami" {
+data "aws_ami" "terrariaform_ami" {
   most_recent = true
   owners      = ["136693071363"] # Debian project owner ID
 
@@ -70,12 +70,12 @@ data "aws_ami" "terrariaform-ami" {
   }
 }
 
-resource "aws_instance" "terrariaform-server" {
-  ami           = data.aws_ami.terrariaform-ami.id
+resource "aws_instance" "terrariaform_server" {
+  ami           = data.aws_ami.terrariaform_ami.id
   instance_type = var.instance_type
   
-  vpc_security_group_ids = [aws_security_group.terrariaform-sg.id]
-  key_name = aws_key_pair.terrariaform-ssh-key.key_name
+  vpc_security_group_ids = [aws_security_group.terrariaform_sg.id]
+  key_name = aws_key_pair.terrariaform_ssh_key.key_name
 
   tags = {
     Name = "terrariaform-server"

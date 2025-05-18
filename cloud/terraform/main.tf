f0718a3c3966cd3f91c9a922a396665628656022
Petri-Hub <fernando.petri01@gmail.com>
2025-05-18 18:59:37 -0300

    feat: finish reserved IP addition

--- cloud/terraform/main.tf
+++ cloud/terraform/main.tf
@@ -13,29 +13,36 @@ provider "aws" {
 
 resource "aws_eip" "terrariaform_elastic_ip" {
   instance = aws_instance.terrariaform_server.id
+  tags = {
+    Name = "terrariaform-elastic-ip"
+  }
 }
 
 resource "aws_security_group" "terrariaform_sg" {
-  name = "terrariaform-sg"
+  name        = "terrariaform-sg"
   description = "Security group that enables SSH and game connections"
+
+  tags = {
+    Name = "terrariaform-sg"
+  }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "terrariaform_ssh_allow" {
-  description = "Allow SSH access"
+  description       = "Allow SSH access"
   security_group_id = aws_security_group.terrariaform_sg.id
   from_port         = 22
   to_port           = 22
-  ip_protocol = "tcp"
-  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "terrariaform_game_allow" {
-  description = "Allow game connections"
+  description       = "Allow game connections"
   security_group_id = aws_security_group.terrariaform_sg.id
   from_port         = 7777
   to_port           = 7777
-  ip_protocol = "tcp"
-  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 resource "aws_vpc_security_group_egress_rule" "terrariaform_all_egress" {
@@ -73,14 +80,14 @@ data "aws_ami" "terrariaform_ami" {
 resource "aws_instance" "terrariaform_server" {
   ami           = data.aws_ami.terrariaform_ami.id
   instance_type = var.instance_type
-  
+
   vpc_security_group_ids = [aws_security_group.terrariaform_sg.id]
-  key_name = aws_key_pair.terrariaform_ssh_key.key_name
+  key_name               = aws_key_pair.terrariaform_ssh_key.key_name
 
   tags = {
     Name = "terrariaform-server"
   }
 
-  user_data = file("../scripts/startup.sh")
+  user_data                   = file("../scripts/startup.sh")
   user_data_replace_on_change = true
 }
